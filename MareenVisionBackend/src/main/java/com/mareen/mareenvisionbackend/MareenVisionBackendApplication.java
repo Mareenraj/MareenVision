52f3c2569a8b8d9cdc757862ee9b98f7ed5f4dce
Mareenraj <mareenraj7v2@gmail.com>
2025-03-27 22:28:50 +0530

    intial backend structure implemented

---
+++ MareenVisionBackend/src/main/java/com/mareen/mareenvisionbackend/MareenVisionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mareen.mareenvisionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareenVisionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MareenVisionBackendApplication.class, args);
+    }
+
+}
---
+++ MareenVisionBackend/src/test/java/com/mareen/mareenvisionbackend/MareenVisionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mareen.mareenvisionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MareenVisionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
